4088a302dae095215ce42c825d71a1433a4acd34
Liran Korn <e_shfiyut@hotmail.com>
2024-02-01 15:38:34 +0200

    adding a new bucket

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -20,6 +20,19 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
+
+resource "aws_s3_bucket_object" "data_object_liran" {
+  bucket = "liran_bucket"
+  key    = "customer-master.xlsx"
+  source = "resources/customer-master.xlsx"
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-customer-master"
+    Environment = local.resource_prefix.value
+    }, {
+    yor_trace            = "b7f01cc7-63d2-41a8-8555-6665e5e39a63"
+  })
+}
+
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
